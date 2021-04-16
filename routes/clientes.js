7fdbc6ff95b3afd7749ac7afb340732cb534bd92
Roger Esteve <rogeresteve30@gmail.com>
2021-04-16 18:00:24 +0200

    login

--- routes/clientes.js
+++ routes/clientes.js
@@ -5,4 +5,12 @@ router.get('/login', (req, res) => {
     res.render('login');
 });
 
+router.post('/login', (req, res) => {
+    const user = req.body.nif;
+    const pass = req.body.pass;
+    
+    if (user == '12345678A' && pass == 'user1234') res.render('index');
+    else res.render('login', {error: true});
+});
+
 module.exports = router;
