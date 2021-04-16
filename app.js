525a1a6bdb4a7749ca04e3f38fe1fcc223e832f3
Roger Esteve <rogeresteve30@gmail.com>
2021-04-16 17:17:15 +0200

    routing

--- app.js
+++ app.js
@@ -11,28 +11,20 @@ app.set("views", path.join(__dirname, "views"));
 
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use('/polizas', require('./routes/polizas'));
+app.use('/partes', require('./routes/partes'));
+app.use('/clientes', require('./routes/clientes'));
+
 app.get('/', (req, res) => {
     res.render('index');
 });
 
-app.post('/presuoueto', (req, res) => {
+app.post('/presupuesto', (req, res) => {
     res.render('presupuesto');
 });
 
 app.get('/info', (req, res) => {
     res.render('info');
-});
-
-app.get('/polizas:(([P])([0-9]{8}))', (req, res) => {
-    res.render('polizas');
-});
-
-app.get('/partes', (req, res) => {
-    res.render('partes');
-});
-
-app.get('/clientes/login', (req, res) => {
-    res.render('login');
 });
 
 app.get('*', (req, res) => {
